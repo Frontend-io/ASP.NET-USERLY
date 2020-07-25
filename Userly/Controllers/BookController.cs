708922c3a2d723804feada4edf2535758e6b0759
Iyobosa Jefferson Osagie <iyobosajefferson2@gmail.com>
2020-07-25 02:52:36 +0100

    Created the books controllers and model

---
+++ Userly/Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Books.Models;
+
+namespace Userly.Controllers
+{
+    public class BookController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Books
+        public ActionResult GetBookData()
+        
+        {
+            //Create a new instance of User
+            var user = new Book()
+            {
+                Title = "Someone to watch over me",
+                BookNumber = 24,
+                Genre = "Student",
+                Year = 2019,
+                Id = 0,
+                Author = "Jefferson"
+            };
+            return View(user);
+        }
+
+        // GET: View book information
+        public ActionResult ViewBook(string title  , int bookId )
+        {
+            ViewBag.title = "You are viewing the book with title ";
+            return View();
+        }
+
+    }
+}
---
+++ Userly/bin/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Books.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public int BookNumber { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+    }
+}
